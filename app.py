6236d3a7f38263da6171d4d657649075ee8c9781
sureshlazaruspaul <sureshlazaruspaul@gmail.com>
2023-09-06 12:21:09 -0400

    edit app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""_summary_
+This code essentially reads a PDF file, extracts its text content, 
+converts it to speech, saves it as an audio file, and plays the 
+audio file, all while handling potential errors during the process.
+"""
+
 # Import required modules
 import pyttsx3
 import PyPDF2
@@ -5,33 +11,36 @@ import os
 
 try:
     # Create PDF reader object
-    pdf = PyPDF2.PdfReader(open('file.pdf', 'rb'))
+    # open the PDF file in binary read mode
+
+    pdf = PyPDF2.PdfReader(open("file.pdf", "rb"))
+
+    # Set output file name
+    outfile = "file.mp3"
 
     # Initialize text-to-speech engine
     speaker = pyttsx3.init()
 
+    clean_text = ""
+
     # Loop through pages
     for page_num in range(len(pdf.pages)):
-
         # Extract text from page
         text = pdf.pages[page_num].extract_text()
 
-        # Clean text by removing newline chars
-        clean_text = text.strip().replace('\n', ' ')
+        # Clean text by removing newline chars & append
+        clean_text = clean_text + text.strip().replace("\n", " ")
 
-        # Print cleaned text
-        print(clean_text)
+    # Print cleaned text
+    print(clean_text)
 
-        # Set output file name
-        outfile = f"page_{str(page_num)}.mp3"
+    # Save audio file
+    speaker.save_to_file(clean_text, outfile)
 
-        # Save audio file
-        speaker.save_to_file(clean_text, outfile)
+    # execute the text-to-speech conversion and wait for it to finish
+    speaker.runAndWait()
 
-        # Run and wait until processing is finished
-        speaker.runAndWait()
-
-    # Clean up
+    # stop the text-to-speech engine
     speaker.stop()
 
     # play file
